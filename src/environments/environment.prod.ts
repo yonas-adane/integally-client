7d6f370bc37155923ee8e36feaec9cc52962a30d
y2ky2k <yonas@Yonass-MacBook-Pro.local>
2021-04-15 17:49:00 -0700

    updated with new api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseURL: 'https://integally-api.herokuapp.com/api/',
-  allowedOrigins:'https://integally-api.herokuapp.com',
+  apiBaseURL: 'https://unity3-prod-api.herokuapp.com/api/',
+  allowedOrigins:'https://unity3-prod-api.herokuapp.com',
   issuer: 'https://dev-268636.okta.com/oauth2/default',
   redirectUri: window.location.origin + '/callback',
   clientId: '0oabodyv4BX7TqDtc4x6',
