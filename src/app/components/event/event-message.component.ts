ece67af2d66c98e680fa1fbef925e5ac326a62af
y2ky2k <yonas.kidanemariam@jewsforjesus.org>
2021-05-13 06:24:00 -0700

    format eventmessage datetime

--- src/app/components/event/event-message.component.ts
+++ src/app/components/event/event-message.component.ts
@@ -141,6 +141,21 @@ ngOnDestroy() {
     this.reQueueForm.reset();
   }
 
+  formatDuration(duration: any) : string {
+    
+    var milliseconds = Math.floor((duration % 1000) / 100),
+      seconds: any = Math.floor((duration / 1000) % 60),
+      minutes: any = Math.floor((duration / (1000 * 60)) % 60),
+      hours: any = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+
+  }
+
   delete(entity: EventMessage): void {
     if (confirm('Are you sure?')) {
       this.eventMessageService.delete(entity).subscribe(() => {
